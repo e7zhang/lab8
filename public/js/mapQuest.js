6b71f0996de05b0747c8ffb97f2225699bdf35a5
Elisa Zhang <e7zhang@ucsd.edu>
2021-02-24 23:40:51 -0800

    lab8

--- public/js/mapQuest.js
+++ public/js/mapQuest.js
@@ -1,3 +1,13 @@
 function initMap() {
 	// add your code here
+	L.mapquest.key = '2JGikhvmYumHCiebDJg3lKZwjKAQkFU6';
+
+	var map = L.mapquest.map('map', {
+		center: [32.87854871709551, -117.23582485557856],
+		layers: L.mapquest.tileLayer('map'),
+		zoom: 12,
+		zoomControl: false
+	});
+
+	L.marker([32.88, -117.236]).addTo(map);
 }
